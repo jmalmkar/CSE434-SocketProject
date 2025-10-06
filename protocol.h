1b8ed35b5519206c97c37df11166be27b7bf7ac6
jmalmkar <jmalmkar@gmail.com>
2025-10-05 20:44:20 -0700

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -7,6 +7,7 @@
 #define MAX_NAME_LEN  32
 #define MAX_MSG_LEN   256
 
+/* Opcodes used in this milestone only */
 enum {
     OP_REGISTER_USER    = 1,
     OP_REGISTER_DISK    = 2,
@@ -14,21 +15,17 @@ enum {
     OP_DEREGISTER_USER  = 4,
     OP_DEREGISTER_DISK  = 5,
     OP_ACK              = 6,
-    OP_ERR              = 7,
-    OP_LS               = 8,
-    OP_PUT_FILE         = 9,
-    OP_READ_FILE        = 10,
-    OP_FAIL_DISK        = 11,
-    OP_DECOMMISSION_DSS = 12
+    OP_ERR              = 7
 };
 
+/* Status codes used in ACK/ERR */
 enum {
-    ST_OK                   = 0,
-    ST_ALREADY_REGISTERED   = 1,
-    ST_NOT_REGISTERED       = 2,
-    ST_INSUFFICIENT_DISKS   = 3,
-    ST_BAD_PARAMS           = 4,
-    ST_INTERNAL             = 5
+    ST_OK                 = 0,
+    ST_ALREADY_REGISTERED = 1,
+    ST_NOT_REGISTERED     = 2,
+    ST_INSUFFICIENT_DISKS = 3,
+    ST_BAD_PARAMS         = 4,
+    ST_INTERNAL           = 5
 };
 
 #pragma pack(push, 1)
@@ -36,14 +33,14 @@ typedef struct {
     uint8_t   version;
     uint8_t   opcode;
     uint16_t  reserved;
-    uint32_t  req_id;
+    uint32_t  req_id;     /* network order */
 } msg_hdr_t;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 typedef struct {
     char      user_name[MAX_NAME_LEN];
-    uint16_t  listen_port;
+    uint16_t  listen_port; /* host order in code; convert at send/recv */
     uint16_t  pad;
 } reg_user_req_t;
 #pragma pack(pop)
@@ -51,16 +48,16 @@ typedef struct {
 #pragma pack(push, 1)
 typedef struct {
     char      disk_name[MAX_NAME_LEN];
-    uint32_t  capacity_blocks;
-    uint16_t  listen_port;
+    uint32_t  capacity_blocks; /* host order in code; convert at send/recv */
+    uint16_t  listen_port;     /* host order in code; convert at send/recv */
     uint16_t  pad;
 } reg_disk_req_t;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 typedef struct {
-    uint32_t  n;
-    uint32_t  block_size;
+    uint32_t  n;          /* host order in code; convert at send/recv */
+    uint32_t  block_size; /* host order in code; convert at send/recv */
 } cfg_dss_req_t;
 #pragma pack(pop)
 
@@ -78,56 +75,13 @@ typedef struct {
 
 #pragma pack(push, 1)
 typedef struct {
-    uint8_t   status;
+    uint8_t   status;    
     uint8_t   _pad[3];
-    char      msg[MAX_MSG_LEN];
+    char      msg[MAX_MSG_LEN]; 
 } resp_t;
 #pragma pack(pop)
 
-#pragma pack(push, 1)
-typedef struct {
-    char dss_name[MAX_NAME_LEN];
-    uint32_t n;
-    uint32_t striping_unit;
-} cfg_dss2_req_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-    char dss_name[MAX_NAME_LEN];
-} ls_req_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-    char dss_name[MAX_NAME_LEN];
-    char filename[64];
-    char owner[MAX_NAME_LEN];
-    uint32_t file_size;
-} put_file_req_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-    char dss_name[MAX_NAME_LEN];
-    char filename[64];
-    char requester[MAX_NAME_LEN];
-} read_file_req_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-    char dss_name[MAX_NAME_LEN];
-    char disk_name[MAX_NAME_LEN];
-} fail_disk_req_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-    char dss_name[MAX_NAME_LEN];
-} decom_dss_req_t;
-#pragma pack(pop)
-
+/* Helpers implemented in protocol.c */
 void proto_make_hdr(msg_hdr_t *h, uint8_t opcode, uint32_t req_id_host);
 
 void proto_user_req_hton(reg_user_req_t *p);
@@ -139,22 +93,4 @@ void proto_disk_req_ntoh(reg_disk_req_t *p);
 void proto_cfg_req_hton(cfg_dss_req_t *p);
 void proto_cfg_req_ntoh(cfg_dss_req_t *p);
 
-void proto_cfg2_req_hton(cfg_dss2_req_t *p);
-void proto_cfg2_req_ntoh(cfg_dss2_req_t *p);
-
-void proto_ls_req_hton(ls_req_t *p);
-void proto_ls_req_ntoh(ls_req_t *p);
-
-void proto_put_req_hton(put_file_req_t *p);
-void proto_put_req_ntoh(put_file_req_t *p);
-
-void proto_read_req_hton(read_file_req_t *p);
-void proto_read_req_ntoh(read_file_req_t *p);
-
-void proto_fail_disk_req_hton(fail_disk_req_t *p);
-void proto_fail_disk_req_ntoh(fail_disk_req_t *p);
-
-void proto_decom_req_hton(decom_dss_req_t *p);
-void proto_decom_req_ntoh(decom_dss_req_t *p);
-
-#endif
+#endif /* PROTOCOL_H */
