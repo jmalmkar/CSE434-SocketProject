afb2cdf1c5412c79061936353c1f5d2555ccc0f6
jmalmkar <jmalmkar@gmail.com>
2025-10-19 20:40:26 -0700

    Refactor protocol.h with new opcodes and structures
    Updated protocol definitions and added new opcodes and structures.

--- protocol.h
+++ protocol.h
@@ -14,24 +14,33 @@
 
 /* Opcodes */
 enum {
-    OP_REGISTER_USER    = 1,
-    OP_REGISTER_DISK    = 2,
-    OP_CONFIGURE_DSS    = 3,
-    OP_DEREGISTER_USER  = 4,
-    OP_DEREGISTER_DISK  = 5,
+    OP_REGISTER_USER      = 1,
+    OP_REGISTER_DISK      = 2,
+    OP_CONFIGURE_DSS      = 3,
+    OP_DEREGISTER_USER    = 4,
+    OP_DEREGISTER_DISK    = 5,
 
-    OP_PUT_OPEN         = 10,
-    OP_PUT_CHUNK        = 11,
-    OP_PUT_CLOSE        = 12,
-    OP_LS               = 13,
-    OP_READ_OPEN        = 14,
-    OP_READ_CHUNK       = 15,
-    OP_READ_CLOSE       = 16,
-    OP_FAIL_DISK        = 17,
-    OP_DECOMMISSION_DSS = 18,
+    /* Named DSS & two-phase operations */
+    OP_LS                 = 10,
 
-    OP_ACK              = 100,
-    OP_ERR              = 101
+    OP_COPY_BEGIN         = 20,
+    OP_COPY_PLAN          = 21,
+    OP_COPY_COMPLETE      = 22,
+
+    OP_READ_BEGIN         = 30,
+    OP_READ_PLAN          = 31,
+    OP_READ_COMPLETE      = 32,
+
+    OP_FAIL_BEGIN         = 40,
+    OP_FAIL_PLAN          = 41,
+    OP_RECOVERY_COMPLETE  = 42,
+
+    OP_DECOM_BEGIN        = 50,
+    OP_DECOM_PLAN         = 51,
+    OP_DECOM_COMPLETE     = 52,
+
+    OP_ACK                = 100,
+    OP_ERR                = 101
 };
 
 /* Status codes used in ACK/ERR */
@@ -45,7 +54,8 @@ enum {
 
     ST_NOT_OWNER          = 6,
     ST_NOT_FOUND          = 7,
-    ST_DSS_NOT_READY      = 8
+    ST_DSS_NOT_READY      = 8,
+    ST_BUSY               = 9
 };
 
 #pragma pack(push, 1)
@@ -57,41 +67,52 @@ typedef struct {
 } msg_hdr_t;
 #pragma pack(pop)
 
+/* Registration */
+
 #pragma pack(push, 1)
 typedef struct {
     char      user_name[MAX_NAME_LEN];
-    uint16_t  listen_port; /* host order in code; convert at send/recv */
-    uint16_t  pad;
+    uint16_t  listen_port;   /* user listen (c-port); host order in code */
+    uint16_t  pad0;
+    uint32_t  ipv4_be;       /* optional; if zero manager uses src */
+    uint16_t  m_port;        /* manager->user port */
+    uint16_t  c_port;        /* peer command port */
 } reg_user_req_t;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 typedef struct {
     char      disk_name[MAX_NAME_LEN];
-    uint32_t  capacity_blocks; /* host order in code; convert at send/recv */
-    uint16_t  listen_port;     /* host order in code; convert at send/recv */
-    uint16_t  pad;
+    uint32_t  capacity_blocks; 
+    uint16_t  listen_port;     
+    uint16_t  pad1;
+    uint32_t  ipv4_be;         /* optional; if zero manager uses src */
+    uint16_t  m_port;          /* manager->disk port */
+    uint16_t  c_port;          /* peer command port */
 } reg_disk_req_t;
 #pragma pack(pop)
 
+/* Configure DSS (Named) */
+
 #pragma pack(push, 1)
 typedef struct {
-    uint32_t  n;          /* host order in code; convert at send/recv */
-    uint32_t  block_size; /* host order in code; convert at send/recv */
+    char      dss_name[16];  
+    uint32_t  n;               /* host order */
+    uint32_t  striping_unit;   /* host order */
 } cfg_dss_req_t;
 #pragma pack(pop)
 
+/* De/Register simple */
+
 #pragma pack(push, 1)
-typedef struct {
-    char user_name[MAX_NAME_LEN];
-} dereg_user_req_t;
+typedef struct { char user_name[MAX_NAME_LEN]; } dereg_user_req_t;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
-typedef struct {
-    char disk_name[MAX_NAME_LEN];
-} dereg_disk_req_t;
+typedef struct { char disk_name[MAX_NAME_LEN]; } dereg_disk_req_t;
 #pragma pack(pop)
+
+/* Generic Response */
 
 #pragma pack(push, 1)
 typedef struct {
@@ -101,54 +122,105 @@ typedef struct {
 } resp_t;
 #pragma pack(pop)
 
-#pragma pack(push, 1)
-typedef struct {
-    char owner[MAX_NAME_LEN];     /* manager infers from sender */
-    char filename[MAX_FILENAME];
-    uint32_t total_size;          /* host order in code */
-} put_open_t;
-#pragma pack(pop)
+/* LS */
 
 #pragma pack(push, 1)
 typedef struct {
-    char filename[MAX_FILENAME];
-    uint32_t seq;                 /* host order in code */
-    uint32_t nbytes;              /* host order in code; <= MAX_CHUNK */
-    uint8_t  data[MAX_CHUNK];
-} put_chunk_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct { char filename[MAX_FILENAME]; } put_close_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct { char owner[MAX_NAME_LEN]; } ls_req_t;
+    /* List all DSSs */  
+    uint8_t dummy;
+} ls_req_t;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 typedef struct {
     uint32_t nbytes;              /* host order */
-    uint8_t  data[512];           /* newline-separated */
+    uint8_t  data[1024];       
 } ls_resp_t;
 #pragma pack(pop)
 
+/* COPY */
+
 #pragma pack(push, 1)
 typedef struct {
+    char dss_name[16];            /* if empty: manager chooses a DSS owned by user */
+    char file_name[MAX_FILENAME];
+    uint32_t file_size;           /* host order */
     char owner[MAX_NAME_LEN];
-    char filename[MAX_FILENAME];
-} read_open_t;
+} copy_begin_t;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 typedef struct {
-    char filename[MAX_FILENAME];
-    uint32_t seq;                 /* host order */
-} read_chunk_req_t;
+    char disk_name[MAX_NAME_LEN];
+    uint32_t ip_be;
+    uint16_t c_port;
+    uint16_t _pad;
+} disk_triplet_t;
 #pragma pack(pop)
 
 #pragma pack(push, 1)
-typedef struct { char filename[MAX_FILENAME]; } read_close_t;
+typedef struct {
+    char dss_name[16];
+    uint32_t n;                   /* host */
+    uint32_t striping_unit;       /* host */
+    disk_triplet_t disks[64];     /* subset used: n */
+} plan_dss_t;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    char dss_name[16];
+    char file_name[MAX_FILENAME];
+    uint32_t file_size;           /* host order */
+    char owner[MAX_NAME_LEN];
+} copy_complete_t;
+#pragma pack(pop)
+
+/* READ */
+
+#pragma pack(push, 1)
+typedef struct {
+    char dss_name[16];
+    char file_name[MAX_FILENAME];
+    char user_name[MAX_NAME_LEN];   /* attempting reader, must equal owner */
+} read_begin_t;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    char dss_name[16];
+    char file_name[MAX_FILENAME];
+    uint32_t file_size;             /* host */
+    plan_dss_t plan;                /* nested plan for convenience */
+} read_plan_t;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    char dss_name[16];
+    char file_name[MAX_FILENAME];
+    char user_name[MAX_NAME_LEN];
+} read_complete_t;
+#pragma pack(pop)
+
+/* FAIL */
+
+#pragma pack(push, 1)
+typedef struct { char dss_name[16]; } fail_begin_t;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct { char dss_name[16]; } recovery_complete_t;
+#pragma pack(pop)
+
+/* DECOMMISSION */
+
+#pragma pack(push, 1)
+typedef struct { char dss_name[16]; } decom_begin_t;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct { char dss_name[16]; } decom_complete_t;
 #pragma pack(pop)
 
 /* Helpers implemented in protocol.c */
