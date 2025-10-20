04e1e4f78195f8adf63770f62d750cbb2a8aaab1
jmalmkar <jmalmkar@gmail.com>
2025-10-19 20:50:12 -0700

    Refactor user.c for logging and buffer size increase
    Updated user.c to enhance send/recv functionality with logging and improved buffer size.

--- user.c
+++ user.c
@@ -3,14 +3,14 @@
 #include "common.h"
 #include "protocol.h"
 
-/* Send a prepared buffer and print response */
-static void send_and_recv(int sock, struct sockaddr_in *mgr, const void *out, size_t outlen,
+/* Common send/recv with milestone logging */
+static void send_and_recv_print(int sock, struct sockaddr_in *mgr, const void *out, size_t outlen,
                           const char *label)
 {
     if (sendto(sock, out, outlen, 0, (struct sockaddr *)mgr, sizeof(*mgr)) < 0)
         DieWithError(label);
 
-    uint8_t inb[2048]; struct sockaddr_in from; socklen_t fromlen=sizeof(from);
+    uint8_t inb[4096]; struct sockaddr_in from; socklen_t fromlen=sizeof(from);
     ssize_t got = recvfrom(sock, inb, sizeof(inb), 0, (struct sockaddr *)&from, &fromlen);
     if (got < (ssize_t)(sizeof(msg_hdr_t))) DieWithError("user: recvfrom failed");
 
@@ -22,9 +22,23 @@ static void send_and_recv(int sock, struct sockaddr_in *mgr, const void *out, si
     } else if (rh.opcode==OP_LS) {
         ls_resp_t lr; memcpy(&lr, inb+sizeof(rh), sizeof(lr));
         printf("user: ls results (%u bytes):\n%.*s", lr.nbytes, (int)lr.nbytes, lr.data);
-    } else if (rh.opcode==OP_READ_CHUNK) {
-        put_chunk_t pc; memcpy(&pc, inb+sizeof(rh), sizeof(pc));
-        fwrite(pc.data, 1, pc.nbytes, stdout);
+    } else if (rh.opcode==OP_COPY_PLAN) {
+        plan_dss_t plan; memcpy(&plan, inb+sizeof(rh), sizeof(plan));
+        printf("user: rx COPY_PLAN dss=%s n=%u striping=%u\n", plan.dss_name, plan.n, plan.striping_unit);
+        for (uint32_t i=0;i<plan.n;i++){
+            struct in_addr a; a.s_addr = plan.disks[i].ip_be;
+            printf("  disk%u %s %s %u\n", i, plan.disks[i].disk_name, inet_ntoa(a), plan.disks[i].c_port);
+        }
+    } else if (rh.opcode==OP_READ_PLAN) {
+        read_plan_t rp; memcpy(&rp, inb+sizeof(rh), sizeof(rp));
+        printf("user: rx READ_PLAN dss=%s file=%s size=%u n=%u striping=%u\n",
+               rp.plan.dss_name, rp.file_name, rp.file_size, rp.plan.n, rp.plan.striping_unit);
+    } else if (rh.opcode==OP_FAIL_PLAN) {
+        plan_dss_t plan; memcpy(&plan, inb+sizeof(rh), sizeof(plan));
+        printf("user: rx FAIL_PLAN dss=%s n=%u\n", plan.dss_name, plan.n);
+    } else if (rh.opcode==OP_DECOM_PLAN) {
+        plan_dss_t plan; memcpy(&plan, inb+sizeof(rh), sizeof(plan));
+        printf("user: rx DECOM_PLAN dss=%s n=%u\n", plan.dss_name, plan.n);
     } else {
         printf("user: rx opcode=%u len=%zd\n", rh.opcode, got);
     }
@@ -33,16 +47,17 @@ static void send_and_recv(int sock, struct sockaddr_in *mgr, const void *out, si
 int main(int argc, char **argv) {
     if (argc < 4) {
         fprintf(stderr,
-          "Usage:\n"
-          "  %s <manager_ip> <manager_port> register-user <user_name> <user_listen_port>\n"
-          "  %s <manager_ip> <manager_port> configure-dss <n> <block_size>\n"
-          "  %s <manager_ip> <manager_port> copy <filename>\n"
-          "  %s <manager_ip> <manager_port> ls <owner>\n"
-          "  %s <manager_ip> <manager_port> read <owner> <filename>\n"
-          "  %s <manager_ip> <manager_port> fail-disk <disk_name>\n"
-          "  %s <manager_ip> <manager_port> decommission-dss\n"
+          "Usage (back-compat + final):\n"
+          "  %s <manager_ip> <manager_port> register-user <user_name> <m_port> <c_port>\n"
+          "  %s <manager_ip> <manager_port> register-user <user_name> <legacy_listen_port>\n"
+          "  %s <manager_ip> <manager_port> configure-dss <dss_name> <n> <striping_unit>\n"
+          "  %s <manager_ip> <manager_port> ls\n"
+          "  %s <manager_ip> <manager_port> copy <dss_name|-> <file_name> <owner>\n"
+          "  %s <manager_ip> <manager_port> read <dss_name> <file_name> <user_name>\n"
+          "  %s <manager_ip> <manager_port> disk-failure <dss_name>\n"
+          "  %s <manager_ip> <manager_port> decommission-dss <dss_name>\n"
           "  %s <manager_ip> <manager_port> deregister-user <user_name>\n",
-          argv[0],argv[0],argv[0],argv[0],argv[0],argv[0],argv[0],argv[0]);
+          argv[0],argv[0],argv[0],argv[0],argv[0],argv[0],argv[0],argv[0],argv[0]);
         exit(1);
     }
     const char *mgr_ip = argv[1];
@@ -50,9 +65,7 @@ int main(int argc, char **argv) {
     maybe_warn_port_range(mgr_port, "user");
 
     int sock;
-    struct sockaddr_in mgr, from;
-    socklen_t fromlen = sizeof(from);
-
+    struct sockaddr_in mgr;
     if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
         DieWithError("user: socket() failed");
 
@@ -64,160 +77,154 @@ int main(int argc, char **argv) {
     const char *cmd = argv[3];
     uint32_t req_id = (uint32_t)time(NULL) ^ (uint32_t)getpid();
 
-    
-    uint8_t out[2048];
+    uint8_t out[4096];
 
     if (strcmp(cmd, "register-user") == 0) {
-        if (argc != 6) { fprintf(stderr, "Bad args for register-user\n"); exit(1); }
+        /* back-compat (5 args) or final (6 args) */
+        if (argc != 6 && argc != 7) { fprintf(stderr, "Bad args for register-user\n"); exit(1); }
         msg_hdr_t h; make_hdr(&h, OP_REGISTER_USER, req_id);
 
         reg_user_req_t p; memset(&p, 0, sizeof(p));
         strncpy(p.user_name, argv[4], MAX_NAME_LEN-1);
-        p.listen_port = htons((unsigned short)atoi(argv[5]));
+        if (argc==6) {
+            p.listen_port = (uint16_t)atoi(argv[5]);
+        } else {
+            p.m_port = (uint16_t)atoi(argv[5]);
+            p.c_port = (uint16_t)atoi(argv[6]);
+        }
 
-        memcpy(out, &h, sizeof(h));
-        memcpy(out + sizeof(h), &p, sizeof(p));
-        printf("user: tx REGISTER_USER req_id=%u name=%s port=%u\n",
-               req_id, argv[4], (unsigned)ntohs(p.listen_port));
-
-        send_and_recv(sock, &mgr, out, sizeof(h)+sizeof(p), "user: send REG_USER failed");
+        memcpy(out, &h, sizeof(h)); memcpy(out + sizeof(h), &p, sizeof(p));
+        printf("user: tx REGISTER_USER req_id=%u name=%s\n", req_id, argv[4]);
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(p),"user: send REG_USER failed");
     }
     else if (strcmp(cmd, "configure-dss") == 0) {
-        if (argc != 6) { fprintf(stderr, "Bad args for configure-dss\n"); exit(1); }
+        if (argc != 7) { fprintf(stderr, "Bad args for configure-dss\n"); exit(1); }
         msg_hdr_t h; make_hdr(&h, OP_CONFIGURE_DSS, req_id);
 
         cfg_dss_req_t p; memset(&p, 0, sizeof(p));
-        p.n          = htonl((uint32_t)strtoul(argv[4], NULL, 10));
-        p.block_size = htonl((uint32_t)strtoul(argv[5], NULL, 10));
+        strncpy(p.dss_name, argv[4], 15);
+        p.n            = (uint32_t)strtoul(argv[5], NULL, 10);
+        p.striping_unit= (uint32_t)strtoul(argv[6], NULL, 10);
 
-        memcpy(out, &h, sizeof(h));
-        memcpy(out + sizeof(h), &p, sizeof(p));
-        printf("user: tx CONFIGURE_DSS req_id=%u n=%s block_size=%s\n", req_id, argv[4], argv[5]);
-
-        send_and_recv(sock, &mgr, out, sizeof(h)+sizeof(p), "user: send CONFIGURE_DSS failed");
+        memcpy(out,&h,sizeof(h)); memcpy(out+sizeof(h),&p,sizeof(p));
+        printf("user: tx CONFIGURE_DSS req_id=%u dss=%s n=%s striping=%s\n", req_id, argv[4], argv[5], argv[6]);
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(p),"user: CONFIGURE_DSS failed");
+    }
+    else if (strcmp(cmd, "ls") == 0) {
+        msg_hdr_t h; make_hdr(&h, OP_LS, req_id);
+        ls_req_t rq; memset(&rq,0,sizeof(rq));
+        memcpy(out,&h,sizeof(h)); memcpy(out+sizeof(h),&rq,sizeof(rq));
+        printf("user: tx LS req_id=%u\n", req_id);
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(rq),"user: LS failed");
     }
     else if (strcmp(cmd, "copy") == 0) {
-        if (argc != 5) { fprintf(stderr, "Bad args for copy\n"); exit(1); }
-        const char *fname = argv[4];
+        if (argc != 7) { fprintf(stderr, "Bad args for copy\n"); exit(1); }
+        const char *dss = argv[4]; 
+        const char *fname = argv[5];
+        const char *owner = argv[6];
 
+        /* Stat file to get size */
         FILE *fp = fopen(fname, "rb");
         if (!fp) { perror("open file"); exit(1); }
         fseek(fp, 0, SEEK_END); long sz = ftell(fp); fseek(fp, 0, SEEK_SET);
-
-        /* PUT_OPEN */
-        msg_hdr_t h; make_hdr(&h, OP_PUT_OPEN, req_id);
-        put_open_t po; memset(&po, 0, sizeof(po));
-        strncpy(po.filename, fname, MAX_FILENAME-1);
-        po.total_size = (uint32_t)sz; /* host order */
-
-        memcpy(out, &h, sizeof(h));
-        memcpy(out + sizeof(h), &po, sizeof(po));
-        printf("user: tx PUT_OPEN req_id=%u file=%s size=%ld\n", req_id, fname, sz);
-        send_and_recv(sock, &mgr, out, sizeof(h)+sizeof(po), "user: PUT_OPEN failed");
-
-        /* PUT_CHUNK loop */
-        uint32_t seq=0;
-        for (;;) {
-            uint8_t buf[MAX_CHUNK];
-            size_t n = fread(buf, 1, MAX_CHUNK, fp);
-
-            msg_hdr_t hc; make_hdr(&hc, OP_PUT_CHUNK, req_id);
-            put_chunk_t pc; memset(&pc, 0, sizeof(pc));
-            strncpy(pc.filename, fname, MAX_FILENAME-1);
-            pc.seq    = seq;             /* host order */
-            pc.nbytes = (uint32_t)n;     /* host order */
-            memcpy(pc.data, buf, n);
-
-            memcpy(out, &hc, sizeof(hc));
-            memcpy(out + sizeof(hc), &pc, sizeof(pc));
-            printf("user: tx PUT_CHUNK req_id=%u file=%s seq=%u n=%zu\n", req_id, fname, seq, n);
-            send_and_recv(sock, &mgr, out, sizeof(hc)+sizeof(pc), "user: PUT_CHUNK failed");
-
-            if (n < MAX_CHUNK) break;
-            seq++;
-        }
         fclose(fp);
 
-        /* PUT_CLOSE */
-        msg_hdr_t ht; make_hdr(&ht, OP_PUT_CLOSE, req_id);
-        put_close_t cl; memset(&cl, 0, sizeof(cl));
-        strncpy(cl.filename, fname, MAX_FILENAME-1);
+        /* Phase 1: COPY_BEGIN -> expect COPY_PLAN */
+        msg_hdr_t h; make_hdr(&h, OP_COPY_BEGIN, req_id);
+        copy_begin_t cb; memset(&cb,0,sizeof(cb));
+        if (strcmp(dss,"-")!=0) strncpy(cb.dss_name,dss,15);
+        strncpy(cb.file_name,fname,MAX_FILENAME-1);
+        cb.file_size = (uint32_t)sz;
+        strncpy(cb.owner,owner,MAX_NAME_LEN-1);
 
-        memcpy(out, &ht, sizeof(ht));
-        memcpy(out + sizeof(ht), &cl, sizeof(cl));
-        printf("user: tx PUT_CLOSE req_id=%u file=%s\n", req_id, fname);
-        send_and_recv(sock, &mgr, out, sizeof(ht)+sizeof(cl), "user: PUT_CLOSE failed");
-    }
-    else if (strcmp(cmd, "ls") == 0) {
-        if (argc != 5) { fprintf(stderr, "Bad args for ls\n"); exit(1); }
-        const char *owner = argv[4];
+        memcpy(out,&h,sizeof(h)); memcpy(out+sizeof(h),&cb,sizeof(cb));
+        printf("user: tx COPY_BEGIN req_id=%u dss=%s file=%s size=%ld owner=%s\n", req_id, dss, fname, sz, owner);
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(cb),"user: COPY_BEGIN failed");
 
-        msg_hdr_t h; make_hdr(&h, OP_LS, req_id);
-        ls_req_t rq; memset(&rq, 0, sizeof(rq));
-        strncpy(rq.owner, owner, MAX_NAME_LEN-1);
+        /* (Data to disks would happen here; simulated.) */
 
-        memcpy(out, &h, sizeof(h));
-        memcpy(out + sizeof(h), &rq, sizeof(rq));
-        printf("user: tx LS req_id=%u owner=%s\n", req_id, owner);
-        send_and_recv(sock, &mgr, out, sizeof(h)+sizeof(rq), "user: LS failed");
+        /* Phase 2: COPY_COMPLETE */
+        msg_hdr_t hc; make_hdr(&hc, OP_COPY_COMPLETE, req_id);
+        copy_complete_t cc; memset(&cc,0,sizeof(cc));
+        if (strcmp(dss,"-")!=0) strncpy(cc.dss_name,dss,15); // safe; manager accepts as chosen previously
+        strncpy(cc.file_name,fname,MAX_FILENAME-1);
+        cc.file_size=(uint32_t)sz;
+        strncpy(cc.owner,owner,MAX_NAME_LEN-1);
+
+        memcpy(out,&hc,sizeof(hc)); memcpy(out+sizeof(hc),&cc,sizeof(cc));
+        printf("user: tx COPY_COMPLETE req_id=%u file=%s\n", req_id, fname);
+        send_and_recv_print(sock,&mgr,out,sizeof(hc)+sizeof(cc),"user: COPY_COMPLETE failed");
     }
     else if (strcmp(cmd, "read") == 0) {
-        if (argc != 6) { fprintf(stderr, "Bad args for read\n"); exit(1); }
-        const char *owner = argv[4];
+        if (argc != 7) { fprintf(stderr, "Bad args for read\n"); exit(1); }
+        const char *dss = argv[4];
         const char *fname = argv[5];
+        const char *uname = argv[6];
 
-        /* READ_OPEN */
-        msg_hdr_t h; make_hdr(&h, OP_READ_OPEN, req_id);
-        read_open_t ro; memset(&ro, 0, sizeof(ro));
-        strncpy(ro.owner, owner, MAX_NAME_LEN-1);
-        strncpy(ro.filename, fname, MAX_FILENAME-1);
+        /* Phase 1: READ_BEGIN -> expect READ_PLAN (size + layout) */
+        msg_hdr_t h; make_hdr(&h, OP_READ_BEGIN, req_id);
+        read_begin_t rb; memset(&rb,0,sizeof(rb));
+        strncpy(rb.dss_name,dss,15);
+        strncpy(rb.file_name,fname,MAX_FILENAME-1);
+        strncpy(rb.user_name,uname,MAX_NAME_LEN-1);
 
-        memcpy(out, &h, sizeof(h));
-        memcpy(out + sizeof(h), &ro, sizeof(ro));
-        printf("user: tx READ_OPEN req_id=%u owner=%s file=%s\n", req_id, owner, fname);
-        send_and_recv(sock, &mgr, out, sizeof(h)+sizeof(ro), "user: READ_OPEN failed");
+        memcpy(out,&h,sizeof(h)); memcpy(out+sizeof(h),&rb,sizeof(rb));
+        printf("user: tx READ_BEGIN req_id=%u dss=%s file=%s user=%s\n", req_id, dss, fname, uname);
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(rb),"user: READ_BEGIN failed");
 
-        for (uint32_t seq=0; seq<128; seq++) {
-            msg_hdr_t hc; make_hdr(&hc, OP_READ_CHUNK, req_id);
-            read_chunk_req_t rq; memset(&rq, 0, sizeof(rq));
-            strncpy(rq.filename, fname, MAX_FILENAME-1);
-            rq.seq = seq; /* host order */
+        /* Phase 2: READ_COMPLETE */
+        msg_hdr_t hc; make_hdr(&hc, OP_READ_COMPLETE, req_id);
+        read_complete_t rc; memset(&rc,0,sizeof(rc));
+        strncpy(rc.dss_name,dss,15);
+        strncpy(rc.file_name,fname,MAX_FILENAME-1);
+        strncpy(rc.user_name,uname,MAX_NAME_LEN-1);
 
-            memcpy(out, &hc, sizeof(hc));
-            memcpy(out + sizeof(hc), &rq, sizeof(rq));
-            printf("user: tx READ_CHUNK req_id=%u file=%s seq=%u\n", req_id, fname, seq);
-            send_and_recv(sock, &mgr, out, sizeof(hc)+sizeof(rq), "user: READ_CHUNK failed");
-        }
-
-        /* READ_CLOSE */
-        msg_hdr_t ht; make_hdr(&ht, OP_READ_CLOSE, req_id);
-        read_close_t rc; memset(&rc, 0, sizeof(rc));
-        strncpy(rc.filename, fname, MAX_FILENAME-1);
-
-        memcpy(out, &ht, sizeof(ht));
-        memcpy(out + sizeof(ht), &rc, sizeof(rc));
-        printf("user: tx READ_CLOSE req_id=%u file=%s\n", req_id, fname);
-        send_and_recv(sock, &mgr, out, sizeof(ht)+sizeof(rc), "user: READ_CLOSE failed");
+        memcpy(out,&hc,sizeof(hc)); memcpy(out+sizeof(hc),&rc,sizeof(rc));
+        printf("user: tx READ_COMPLETE req_id=%u file=%s\n", req_id, fname);
+        send_and_recv_print(sock,&mgr,out,sizeof(hc)+sizeof(rc),"user: READ_COMPLETE failed");
     }
-    else if (strcmp(cmd, "fail-disk") == 0) {
-        if (argc != 5) { fprintf(stderr, "Bad args for fail-disk\n"); exit(1); }
-        const char *dname = argv[4];
+    else if (strcmp(cmd, "disk-failure") == 0) {
+        if (argc != 5) { fprintf(stderr, "Bad args for disk-failure\n"); exit(1); }
+        const char *dss = argv[4];
 
-        msg_hdr_t h; make_hdr(&h, OP_FAIL_DISK, req_id);
-        dereg_disk_req_t rq; memset(&rq, 0, sizeof(rq));
-        strncpy(rq.disk_name, dname, MAX_NAME_LEN-1);
+        /* FAIL_BEGIN -> expect FAIL_PLAN; then RECOVERY_COMPLETE */
+        msg_hdr_t h; make_hdr(&h, OP_FAIL_BEGIN, req_id);
+        fail_begin_t fb; memset(&fb,0,sizeof(fb));
+        strncpy(fb.dss_name,dss,15);
 
-        memcpy(out, &h, sizeof(h));
-        memcpy(out + sizeof(h), &rq, sizeof(rq));
-        printf("user: tx FAIL_DISK req_id=%u disk=%s\n", req_id, dname);
-        send_and_recv(sock, &mgr, out, sizeof(h)+sizeof(rq), "user: FAIL_DISK failed");
+        memcpy(out,&h,sizeof(h)); memcpy(out+sizeof(h),&fb,sizeof(fb));
+        printf("user: tx FAIL_BEGIN req_id=%u dss=%s\n", req_id, dss);
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(fb),"user: FAIL_BEGIN failed");
+
+        /* simulate recovery */
+        msg_hdr_t hc; make_hdr(&hc, OP_RECOVERY_COMPLETE, req_id);
+        recovery_complete_t rc; memset(&rc,0,sizeof(rc));
+        strncpy(rc.dss_name,dss,15);
+
+        memcpy(out,&hc,sizeof(hc)); memcpy(out+sizeof(hc),&rc,sizeof(rc));
+        printf("user: tx RECOVERY_COMPLETE req_id=%u dss=%s\n", req_id, dss);
+        send_and_recv_print(sock,&mgr,out,sizeof(hc)+sizeof(rc),"user: RECOVERY_COMPLETE failed");
     }
     else if (strcmp(cmd, "decommission-dss") == 0) {
-        msg_hdr_t h; make_hdr(&h, OP_DECOMMISSION_DSS, req_id);
+        if (argc != 5) { fprintf(stderr, "Bad args for decommission-dss\n"); exit(1); }
+        const char *dss = argv[4];
 
-        memcpy(out, &h, sizeof(h));
-        printf("user: tx DECOMMISSION_DSS req_id=%u\n", req_id);
-        send_and_recv(sock, &mgr, out, sizeof(h), "user: DECOMMISSION_DSS failed");
+        msg_hdr_t h; make_hdr(&h, OP_DECOM_BEGIN, req_id);
+        decom_begin_t db; memset(&db,0,sizeof(db));
+        strncpy(db.dss_name,dss,15);
+
+        memcpy(out,&h,sizeof(h)); memcpy(out+sizeof(h),&db,sizeof(db));
+        printf("user: tx DECOM_BEGIN req_id=%u dss=%s\n", req_id, dss);
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(db),"user: DECOM_BEGIN failed");
+
+        /* simulate delete operations */
+        msg_hdr_t hc; make_hdr(&hc, OP_DECOM_COMPLETE, req_id);
+        decom_complete_t dc; memset(&dc,0,sizeof(dc));
+        strncpy(dc.dss_name,dss,15);
+
+        memcpy(out,&hc,sizeof(hc)); memcpy(out+sizeof(hc),&dc,sizeof(dc));
+        printf("user: tx DECOM_COMPLETE req_id=%u dss=%s\n", req_id, dss);
+        send_and_recv_print(sock,&mgr,out,sizeof(hc)+sizeof(dc),"user: DECOM_COMPLETE failed");
     }
     else if (strcmp(cmd, "deregister-user") == 0) {
         if (argc != 5) { fprintf(stderr, "Bad args for deregister-user\n"); exit(1); }
@@ -226,11 +233,9 @@ int main(int argc, char **argv) {
         dereg_user_req_t p; memset(&p, 0, sizeof(p));
         strncpy(p.user_name, argv[4], MAX_NAME_LEN-1);
 
-        memcpy(out, &h, sizeof(h));
-        memcpy(out + sizeof(h), &p, sizeof(p));
+        memcpy(out, &h, sizeof(h)); memcpy(out + sizeof(h), &p, sizeof(p));
         printf("user: tx DEREGISTER_USER req_id=%u name=%s\n", req_id, argv[4]);
-
-        send_and_recv(sock, &mgr, out, sizeof(h)+sizeof(p), "user: send DEREG failed");
+        send_and_recv_print(sock,&mgr,out,sizeof(h)+sizeof(p),"user: send DEREG failed");
     }
     else {
         fprintf(stderr, "Unknown command: %s\n", cmd);
@@ -240,4 +245,3 @@ int main(int argc, char **argv) {
     close(sock);
     return 0;
 }
-
